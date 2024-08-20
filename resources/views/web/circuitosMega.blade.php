be8846ae540907b18b53cd1cb00a5af68152f3d8
programadorbt1 <programadorbt1@gmail.com>
2024-08-20 16:32:28 -0500

    Se agrega el logo en detalle megatravel

--- resources/views/web/circuitosMega.blade.php
+++ resources/views/web/circuitosMega.blade.php
@@ -35,7 +35,7 @@
                             <div class="sidebar-blog__single sidebar-blog__single--posts wow animated fadeInUp"
                                 data-wow-delay="0.1s" data-wow-duration="1500ms">
                                 <h4 class="sidebar-blog__title text-center">
-                                    <img style="width: 150px" src="{{ asset('cucapah/img/logo-cucapah-mobile.webp') }}"
+                                    <img style="width: 150px" src="{{ asset('angie/img/logo.png') }}"
                                         alt="Logo {{ $nameEnterprise }}">
                                 </h4>
                                 <ul class="sidebar-blog__posts ">
